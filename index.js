60162ea6175eb31c9fd9a87c4529c2640e8e74d9
jojo051 <youpi051@yahoo.fr>
2026-01-01 18:35:04 +0100

    restore cron after noel

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ client.on("messageCreate", (message) => {
 client.once("ready", () => {
 	console.log(`Bot connecté freeEpic en tant que ${client.user.tag}`);
 	//0 18 * * 4
-	cron.schedule("0 18 * * *", async () => {
+	cron.schedule("0 18 * * 4", async () => {
 		console.log("🔔 Vérification des jeux gratuits Epic Games...");
 
 		const freeGames = await getFreeEpicGames();
